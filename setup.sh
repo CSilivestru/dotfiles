1bb5935c923efd091ad4e3cdd62f840af64962ad
Chris Silivestru <chris.silivestru@shopify.com>
2021-02-25 11:27:31 -0500

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 SRC_BASH_RC="$(realpath .bashrc)"
 SRC_BASH_PROFILE="$(realpath .bash_profile)"
 SRC_GIT_COMPLETION="$(realpath git-completion.bash)"
+SRC_GIT_CONFIG="$(realpath .gitconfig)"
 DST_BASH_RC="${HOME}/.bashrc"
 DST_BASH_PROFILE="${HOME}/.bash_profile"
 DST_GIT_COMPLETION="${HOME}/git-completion.bash"
+DST_GIT_CONFIG="${HOME}/.gitconfig"
 
 # Link to $HOME
 if [ ! -f ${DST_BASH_RC} ]; then
